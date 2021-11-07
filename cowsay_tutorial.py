caa95af545d25001afa7adc34ad374bb18fc817e
Sebastian Calleja <72355114+sebcalleja@users.noreply.github.com>
2021-11-07 10:33:49 -0700

    Add files via upload

---
+++ cowsay_tutorial.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+
+import cowsay
+cowsay.cow('Hello World')
+
+print('********************')
+print('********************')
+print('********************')
+print('********************')
+print('********************')
+print('********************')
+print('********************')
+print()
+print('Looking Good!')
+print()
+print('********************')
+print('********************')
+print('********************')
+print('********************')
+print('********************')
+print('********************')
+print('********************')
+
+
+print(cowsay.get_output_string('trex', 'Hello (extinct) World'))
